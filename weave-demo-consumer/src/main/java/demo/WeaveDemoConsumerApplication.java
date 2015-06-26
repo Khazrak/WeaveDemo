f4fc6155ea58379133dd70f90058cb569ec477cb
Karl (llm471) <karl.z.andersson@teliasonera.se>
2015-06-26 08:57:54 +0200

    CM-1: Added inial Boot-projects for the Demo Microservices

---
+++ weave-demo-consumer/src/main/java/demo/WeaveDemoConsumerApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaveDemoConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeaveDemoConsumerApplication.class, args);
+    }
+}
